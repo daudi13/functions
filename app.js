a895a8d8a81461ba8ba20e78b6c8ceddfa3d3b40
daudi13 <davouma13@gmail.com>
2021-12-21 17:20:08 +0300

    commit changes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const poll = {
     question: "What is your favourite programming language?",
     options: ["0: JavaScript", "1: Python", "2:Rust", "3: C++"],
-    answers: [0, 0, 0, 0]
+    answers: [0, 0, 0, 0],
+    registerNewAnswer() {
+        const ans = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(write option number)`));
+
+        //register
+
+        typeof ans === 'number' && ans < this.answers.length && this.answers[ans]++
+
+        this.displayResult('string');
+    },
+
+    displayResult(type = 'array') {
+        
+        if (type === 'array') {
+            console.log(this.answers)
+        } else if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`)
+        }
+    }
 }
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
 
 
 /*A poll has a question, an araay of options from which people can choose, and an array with the number of replies for each option. this data is stored in the starter poll above*/
@@ -22,36 +42,3 @@ based on the input number, update the 'answers' array property. for example. if 
 
 
 */
-
-
-
-poll.registerNewAnswer = function () {
-
-    //GET THE ANSWER
-    const ans = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
-
-    //register
-    
-    typeof ans === 'number' && ans < this.answers.length && this.answers[ans]++;
-
-    this.displayResult();
-    this.displayResult('string');
-    
-}
-
-poll.displayResult = function (type = 'array') {
-
-    if (type === 'array') {
-        console.log(this.answers)
-    } else if (type === 'string') {
-        console.log(`Poll results are ${this.answers.join(', ')}`)
-    }
-}
-
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
-
-
-
-// const arr = [1, 2, 3, 4];
-
-// console.log(arr.join(', '))
